0ddf77d42a6e238cd69bee30f6eed19be62c1cce
Ben Wiles <benjay126@gmail.com>
2022-07-11 23:41:50 +1200

    added script

--- index.php
+++ index.php
@@ -36,13 +36,5 @@
         </thead>
     </table>
 </body>
-<script>
-    for (var i = 0; i < document.getElementById('table').children.length; i++) {
-        document.getElementById('table').children.addEventListener('click', (e) => {
-            var id = e.target.children[1].value
-            console.log(id);
-            window.location.href = `https://matteodimaio.net/rice/rice-purity-leaderboard/user_connect.php?id=${id}`;
-        });
-    }​
-</script>
+<script src='user_redirect.js' />
 </html>
---
+++ user_redirect.js
@@ -0,0 +1,7 @@
+﻿for (var i = 0; i < document.getElementById('table').children.length; i++) {
+    document.getElementById('table').children.addEventListener('click', (e) => {
+        var id = e.target.children[1].value
+        console.log(id);
+        window.location.href = `https://matteodimaio.net/rice/rice-purity-leaderboard/user_connect.php?id=${id}`;
+    });
+}​
