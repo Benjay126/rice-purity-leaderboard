d576bda355c6ea405db82b6d37377885a6f63af8
Ben Wiles <benjay126@gmail.com>
2022-07-11 18:29:34 +1200

    sql test

--- user_connect.php
+++ user_connect.php
@@ -3,7 +3,7 @@
 	$host = '172.105.173.88';
 	$username = 'ben';
 	$sql_pw = 'unhinge-naming-whoever-carnation-improving';
-	$conn = mysql_connect($host, $username, $sql_pw);
+	$conn = mysql_connect($host, $username, $sql_pw) or die("Unable to connect to '$host'");;
 
 	$pw = $_POST['total-pin'];
 	echo $pw;
