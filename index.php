00ee33301c1dc0681774b9f4934c37244e2a4948
Ben Wiles <benjay126@gmail.com>
2022-07-11 21:10:43 +1200

    php removed

--- index.php
+++ index.php
@@ -5,12 +5,6 @@
     <meta charset="utf-8" />
     <title>Rice Purity Leaderboard</title>
     <link rel="stylesheet" href="style.css" />
-    <?php 
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL)
-        require('sql_connect.php');  
-     ?>
 </head>
 <body>
     <header>Rice Purity Leaderboard</header>
@@ -23,19 +17,7 @@
             <th>% Change</th>
         </tr>
         
-		<?php
-			$sql = "SELECT * FROM scores";
-			$result = $conn->query($sql);
-			
-			if ($result->num_rows > 0) {
-			    while($row = $result->fetch_assoc()) {
-                    $score_change = round((($row['init_score'] / $row['curr_score']) - 1) * 100);
-                    echo '<tr><td></td><th>'.$row['name'].'</th><th>'.$row['init_score'].'</th><th>'.$row['curr_score'].'</th><th>'.$score_change.'%</td></tr>';
-			    }
-			} else {
-			    echo "0 results";
-			}
-		?>
+    
         
     </table>
 </body>
