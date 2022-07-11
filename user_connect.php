fec4f3afba4aef45e50dafdfb47f362c26062a9f
Ben Wiles <benjay126@gmail.com>
2022-07-11 18:47:11 +1200

    php test

--- user_connect.php
+++ user_connect.php
@@ -17,10 +17,9 @@
 		$username = 'ben';
 		$sql_pw = 'unhinge-naming-whoever-carnation-improving';
 
-		//$conn = mysql_connect($host, $username, $sql_pw) or die("Unable to connect to '$host'");
+		$conn = mysql_connect($host, $username, $sql_pw) or die("Unable to connect to '$host'");
 	
 		$pw = $_POST['total-pin'];
-		echo $name;
 		echo $pw;
 	?>
 </body>
