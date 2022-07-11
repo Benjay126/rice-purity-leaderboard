2b0745c125780e26af92f11d8a5942176b284bb9
Ben Wiles <benjay126@gmail.com>
2022-07-11 21:29:56 +1200

    php

--- user_connect.php
+++ user_connect.php
@@ -6,7 +6,18 @@
     <meta charset="utf-8" />
     <title>Rice Purity Leaderboard</title>
     <link rel="stylesheet" href="style.css" />
-	<?php require('sql_connect.php'); ?>
+	<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL)
+    $host = 'localhost:3306';
+    $username = 'ben';
+    $sql_pw = 'unhinge-naming-whoever-carnation-improving';
+    $db = 'rice_purity'
+    $conn = mysqli_connect($host, $username, $sql_pw, $db) or die("Unable to connect to '$host'");
+    $pw = $_POST['total-pin'];
+    $id = $_POST['id'];   
+?>
 </head>
 <body>
     <header id="title">Rice Purity Record - <?php echo $id ?></header>
