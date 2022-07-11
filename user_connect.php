12980a505aa311a4c6f1567ffb29f997f71b91f0
Ben Wiles <benjay126@gmail.com>
2022-07-11 17:57:50 +1200

    sql test

--- user_connect.php
+++ user_connect.php
@@ -2,12 +2,6 @@
 	$host = '172.105.173.88';
 	$username = 'ben';
 	$pw = 'unhinge-naming-whoever-carnation-improving';
-	$conn = new mysql($host, $username, $pw);
-
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-	echo "Connected successfully";
 
 	$pw = $_POST['total-pin'];
 	echo $pw;
