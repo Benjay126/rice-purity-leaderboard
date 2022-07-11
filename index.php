fee20495ef53d7ecf2df924ee32902dbcf673502
Ben Wiles <benjay126@gmail.com>
2022-07-11 23:19:38 +1200

    row edits

--- index.php
+++ index.php
@@ -5,12 +5,7 @@
     <meta charset="utf-8" />
     <title>Rice Purity Leaderboard</title>
     <link rel="stylesheet" href="style.css" />
-    <?php 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-    require('sql_connect.php');
-    ?>
+    <?php require('sql_connect.php'); ?>
 </head>
 <body>
     <header>Rice Purity Leaderboard</header>
@@ -22,7 +17,6 @@
             <th>Curr. Score</th>
             <th>% Change</th>
         </tr>
-        <form>
 		<?php
 			$sql = "SELECT * FROM scores";
 			$result = $conn->query($sql);
@@ -30,13 +24,12 @@
 			if ($result->num_rows > 0) {
 			    while($row = $result->fetch_assoc()) {
                     $score_change = round((($row['init_score'] / $row['curr_score']) - 1) * 100);
-                    echo '<button type="submit"><tr><td></td><th>'.strtoupper($row['name']).'</th><th>'.$row['init_score'].'</th><th>'.$row['curr_score'].'</th><th>'.$score_change.'%</td></tr></button>';
+                    echo '<tr><td></td><th>'.strtoupper($row['name']).'</th><th>'.$row['init_score'].'</th><th>'.$row['curr_score'].'</th><th>'.$score_change.'%</td></tr>';
 			    }
 			} else {
 			    echo "0 results";
 			}
 		?>
-        </form>
     </table>
 </body>
 </html>
