42b05c727e6fbc6d0951526fc2af19d37bb5b6bc
Ben Wiles <benjay126@gmail.com>
2022-07-11 19:37:42 +1200

    checkboxed questions

--- user_connect.php
+++ user_connect.php
@@ -2,12 +2,6 @@
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="utf-8" />
-    <title>Rice Purity Leaderboard</title>
-    <link rel="stylesheet" href="style.css" />
-</head>
-<body>
-    <header id="title">Rice Purity Record - ###</header>
 	<?php
 		ini_set('display_errors', 1);
 		ini_set('display_startup_errors', 1);
@@ -19,25 +13,34 @@
 		$db = 'rice_purity';
 
 		$conn = mysqli_connect($host, $username, $sql_pw, $db) or die("Unable to connect to '$host'");
-	
-		$sql = "SELECT * FROM questions";
-		$result = $conn->query($sql);
-		
-		if ($result->num_rows > 0) {
-		  // output data of each row
-		  while($row = $result->fetch_assoc()) {
-		    echo "id: " . $row["id"]. " - question: " . $row["question"];
-		  }
-		} else {
-		  echo "0 results";
-		}
-		$conn->close();
+	?>
+    <meta charset="utf-8" />
+    <title>Rice Purity Leaderboard</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <header id="title">Rice Purity Record - ###</header>
+	<div id="questions">
+		<ol>
+			<?php
+				$sql = "SELECT * FROM questions";
+				$result = $conn->query($sql);
+				
+				if ($result->num_rows > 0) {
+				  // output data of each row
+				  while($row = $result->fetch_assoc()) {
+				    echo '<li><input type="checkbox" id=' . $row['id'] . '>' . $row["question"] . '</li>';
+				  }
+				} else {
+				  echo "0 results";
+				}
+				$conn->close();
 
-		$pw = $_POST['total-pin'];
-		echo $pw;
-	?>
-	<?php
-		echo $pw;
-	?>
+				$pw = $_POST['total-pin'];
+				echo $pw;
+			?>
+		</ol>
+	</div>
+	
 </body>
 </html>
