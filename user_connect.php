85147b8c565f29ca2fa33c53de5862703dbeffee
Ben Wiles <benjay126@gmail.com>
2022-07-11 18:02:15 +1200

    php test

--- user_connect.php
+++ user_connect.php
@@ -4,11 +4,6 @@
 	$sql_pw = 'unhinge-naming-whoever-carnation-improving';
 	$conn = new mysql($host, $username, $pw);
 
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-	echo "Connected successfully";
-
 	$pw = $_POST['total-pin'];
 	echo $pw;
 	echo 'test';
