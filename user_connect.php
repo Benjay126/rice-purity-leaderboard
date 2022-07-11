7c8713456201b447ab8514726266d364d575b6a3
Ben Wiles <benjay126@gmail.com>
2022-07-12 00:45:24 +1200

    comment

--- user_connect.php
+++ user_connect.php
@@ -22,7 +22,7 @@
             <input type="number" id='pin-2' name='pin-2' oninput='shiftFocus(3)'>
             <input type="number" id='pin-3' name='pin-3' oninput='shiftFocus(4)'>
             <input type="number" id='pin-4' name='pin-4' oninput='joinPin()'>
-            <!--<input type="submit">-->
+            <input type="submit">
             <input style='visibility: hidden' type="number" id='total-pin' name='total-pin'>
             <input style='visibility: hidden' type="text" id='id' name='id'>
         </div>
