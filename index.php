96738bed04c1ae67aa2c086fd3eeeffd57c8a64b
Ben Wiles <benjay126@gmail.com>
2022-07-12 15:02:53 +1200

    php undo

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <title>Rice Purity Leaderboard</title>
     <link rel="stylesheet" href="style.css" />
-    <?php require('sql_connect.php'); ?>
+    <?php require('sql_connect.php');  ?>
 </head>
 <body>
     <header>Rice Purity Leaderboard</header>
@@ -19,29 +19,22 @@
             </tr>
         </thead>
         <tbody id="table">
+		<?php
+	        $sql = "SELECT name, init_score, curr_score FROM users";
+	        $result = $conn->query($sql);
+			if ($result->num_rows > 0) {
+			    while($row = $result->fetch_assoc()) {
+                    $score_change = round((($row['init_score'] / $row['curr_score']) - 1) * 100);
+                    echo '<tr><td></td><th>'.strtoupper($row['name']).'</th><th>'.$row['init_score'].'</th><th>'.$row['curr_score'].'</th><th>'.$score_change.'%</td></tr>';
+			    }
+			} else {
+			    echo "0 results";
+			}
+		?>
         </thead>
     </table>
 </body>
 <script>
-    var users = [];
-	<?php
-	    $sql = "SELECT name, init_score, curr_score FROM users";
-	    $result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) { ?>
-                users.push({
-                     'id': <?php echo $row['id']; ?>,
-                     'init_score': <?php echo $row['init_score']; ?>,
-                     'curr_score': <?php echo $row['curr_score']; ?>,
-                     'score_change': <?php round((($row['init_score'] / $row['curr_score']) - 1) * 100); ?>
-                });
-		    <?php}
-		} else {
-		    echo "0 results";
-		}
-	?>
-
-    var row = '<tr><td></td><th></th><th></th><th></th><th>%</td></tr>';
     for(i = 0; i < document.getElementById('table').children.length; i++) {
         document.getElementById('table').children[i].addEventListener('click', (e) => {
             console.log(e.target);
