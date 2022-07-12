6e672c60bbf27f969adb854cf0a4890b6deed57d
Ben Wiles <benjay126@gmail.com>
2022-07-12 19:55:36 +1200

    change style

--- questions.php
+++ questions.php
@@ -27,28 +27,29 @@
 </head>
 <body>
     <header id="title">Rice Purity Record - <?php echo $_GET['id'] ?></header>
-	<form style="margin: auto; width: 100%; font-size: 20px; margin-top: 40px;" method="post">
-		<ol>
-			<?php
-				$a_query = "SELECT answers FROM users WHERE name='".$_GET['id']."'";
-				$a_result = $conn->query($a_query);
+	<div class="main-content">
+		<form style="margin: auto; width: 100%; font-size: 20px; margin-top: 40px;" method="post">
+			<ol>
+				<?php
+					$a_query = "SELECT answers FROM users WHERE name='".$_GET['id']."'";
+					$a_result = $conn->query($a_query);
 
-				$q_query = "SELECT * FROM questions";
-				$q_result = $conn->query($q_query);
-				
-				if ($q_result->num_rows > 0 and $a_result->num_rows > 0) {
-					$answer = $a_result->fetch_assoc();
-					$a_array = str_split($answer['answers']);
-					while($row = $q_result->fetch_assoc()) {
-						echo '<li><input type="checkbox" id="'.$row['id'].'" name="'.$row['id'].'"'.($a_array[$row['id'] - 1] == 1 ? ' checked' : '').'> '.$row["question"].'</li>';
+					$q_query = "SELECT * FROM questions";
+					$q_result = $conn->query($q_query);
+					
+					if ($q_result->num_rows > 0 and $a_result->num_rows > 0) {
+						$answer = $a_result->fetch_assoc();
+						$a_array = str_split($answer['answers']);
+						while($row = $q_result->fetch_assoc()) {
+							echo '<li><input type="checkbox" id="'.$row['id'].'" name="'.$row['id'].'"'.($a_array[$row['id'] - 1] == 1 ? ' checked' : '').'> '.$row["question"].'</li>';
+						}
+					} else {
+						echo "0 results";
 					}
-				} else {
-					echo "0 results";
-				}
-			?>
-		</ol>
-		<input type="submit" name="submit">
-    </form>
+				?>
+			</ol>
+			<input type="submit" name="submit">
+		</form>
 	</div>
 </body>
 </html>
