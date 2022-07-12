dd1f96925abb3ac716db75b0ecb457a9cddfe268
Ben Wiles <benjay126@gmail.com>
2022-07-12 17:42:45 +1200

    html error

--- new_user.php
+++ new_user.php
@@ -19,7 +19,7 @@
         <div>
             <input type="text" maxlength=1 id='id-pin-1' name='pin-1' oninput='shiftFocus(2, "id")'>
             <input type="text" maxlength=1 id='id-pin-2' name='pin-2' oninput='shiftFocus(3, "id")'>
-            <input type="text" maxlength=1 id='id-pin-4' name='pin-4' oninput='joinPin(3)'>
+            <input type="text" maxlength=1 id='id-pin-3' name='pin-4' oninput='joinPin(3)'>
             <input style='visibility: hidden' type="text" id='id' name='id'>
         </div>
         <div>
