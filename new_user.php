b63e1df9c8bfb386d91c9c7e17fe519931b2de65
Ben Wiles <benjay126@gmail.com>
2022-07-12 17:40:33 +1200

    form test

--- new_user.php
+++ new_user.php
@@ -14,8 +14,63 @@
 </head>
 <body>
     <header>Rice Purity Leaderboard - New User</header>
-    <form id='form' name='form' method="post"></form>
+    <form id='form' name='form' method="post">
+        <label>User </label>
+        <div>
+            <input type="text" maxlength=1 id='id-pin-1' name='pin-1' oninput='shiftFocus(2, "id")'>
+            <input type="text" maxlength=1 id='id-pin-2' name='pin-2' oninput='shiftFocus(3, "id")'>
+            <input type="text" maxlength=1 id='id-pin-4' name='pin-4' oninput='joinPin()'>
+            <input style='visibility: hidden' type="text" id='id' name='id'>
+        </div>
+        <div>
+            <input type="number" maxlength=1 id='user-pin-1' name='pin-1' oninput='shiftFocus(2, "user")'>
+            <input type="number" maxlength=1 id='user-pin-2' name='pin-2' oninput='shiftFocus(3, "user")'>
+            <input type="number" maxlength=1 id='user-pin-3' name='pin-3' oninput='shiftFocus(4, "user")'>
+            <input type="number" maxlength=1 id='user-pin-4' name='pin-4' oninput='joinPin()'>
+            <input style='visibility: hidden' type="number" id='total-admin-pin' name='total-admin-pin'>
+        </div>
+        <div>
+            <input type="number" maxlength=1 id='admin-pin-1' name='pin-1' oninput='shiftFocus(2, "admin")'>
+            <input type="number" maxlength=1 id='admin-pin-2' name='pin-2' oninput='shiftFocus(3, "admin")'>
+            <input type="number" maxlength=1 id='admin-pin-3' name='pin-3' oninput='shiftFocus(4, "admin")'>
+            <input type="number" maxlength=1 id='admin-pin-4' name='pin-4' oninput='joinPin()'>
+            <input style='visibility: hidden' type="number" id='total-admin-pin' name='total-admin-pin'>
+        </div>
+            <input type="hidden" value="submit" name="btnSubmit">
+    </form>
 </body>
+<script>
+    document.getElementById('id-pin-1').focus();
+
+    function squarePins(pinType) {
+        for(i = 1; i < 5; i++) {
+            var p = document.getElementById(`${pinType}-pin-${i}`);
+            dimDiff = p.offsetWidth - p.offsetHeight;
+            p.style.paddingTop = (dimDiff / 2) + 'px';
+            p.style.paddingBottom = (dimDiff / 2) + 'px';
+        }
+    }
+    window.addEventListener('resize', squarePins('id'));
+    window.addEventListener('resize', squarePins('user'));
+    window.addEventListener('resize', squarePins('admin'));
+    squarePins('id');
+    squarePins('user');
+    squarePins('admin');
+
+    function shiftFocus(num, pinType) { document.getElementById(`${pinType}-pin-${num}`).focus(); }
+
+    function joinPin() {
+        const pin = [
+            document.getElementById('pin-1').value,
+            document.getElementById('pin-2').value,
+            document.getElementById('pin-3').value,
+            document.getElementById('pin-4').value
+        ];
+        var pinStr = pin.join('');
+        document.getElementById('total-pin').value = pinStr;
+        //document.getElementById('form').submit();
+    }
+</script>
 </html>
 <?php
     if(isset($_POST['btnSubmit'])) {
