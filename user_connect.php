02f81f368cc89201753f5782ed028eceec7e5452
Ben Wiles <benjay126@gmail.com>
2022-07-12 16:12:14 +1200

    php error

--- user_connect.php
+++ user_connect.php
@@ -62,7 +62,7 @@
 </script>
 </html>
 <?php
-    if(isset($_POST['submit'])) {
+    if(isset($_POST['btnSubmit'])) {
         $sql = "SELECT * FROM users WHERE name='".$_POST['id']."'";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
