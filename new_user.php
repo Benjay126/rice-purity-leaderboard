fa80c72bd1a292b019239aa40fa9fd9dc738ccf7
Ben Wiles <benjay126@gmail.com>
2022-07-25 11:10:24 +1200

    js square and pin submit

--- new_user.php
+++ new_user.php
@@ -43,6 +43,23 @@
     </form>
 </body>
 <script>
+    function shiftFocus(num, pinType) { document.getElementById(`${pinType}-pin-${num}`).focus(); }
+    function squarePins(numPins, pinType) {
+        for (i = 1; i <= numPins; i++) {
+            var p = document.getElementById(`${pinType}-pin-${i}`);
+            dimDiff = p.offsetWidth - p.offsetHeight;
+            p.style.paddingTop = (dimDiff / 2) + 'px';
+            p.style.paddingBottom = (dimDiff / 2) + 'px';
+        }
+    }
+    function joinPin(numPins, pinType) {
+        const pin = [];
+        for (i = 1; i <= numPins; i++) { pin.push(document.getElementById(`${pinType}-pin-${i}`).value); }
+        var pinStr = pin.join('');
+        document.getElementById(`total-${pinType}-pin`).value = pinStr;
+        document.getElementById('form').submit();
+    }
+
     document.getElementById('id-pin-1').focus();
     window.addEventListener('resize', squarePins(3, 'id'));
     window.addEventListener('resize', squarePins(4, 'user'));
