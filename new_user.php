c9bbdb7a8cce00ba61d02b6e1dce7f7156a799ca
Ben Wiles <benjay126@gmail.com>
2022-07-25 15:03:17 +1200

    php error

--- new_user.php
+++ new_user.php
@@ -110,7 +110,7 @@
             curl_setopt($ch, CURLOPT_POST, 1);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            //$response = curl_exec($ch);
+            $response = curl_exec($ch);
             curl_close($ch);
         } else {
             echo "User already added.";
